4612b0c13702726dca82a3f3a0505c44226d30b0
JazzXHiro <73639405+JazzXHiro@users.noreply.github.com>
2025-03-27 01:42:27 +0530

    added bucket movement with world boundaries, dropSprite and randomized dropSprite spawn(multiple DropGlitch)

--- core/src/main/java/com/badlogic/drop/Main.java
+++ core/src/main/java/com/badlogic/drop/Main.java
@@ -9,6 +9,9 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
@@ -24,6 +27,12 @@ public class Main implements ApplicationListener {
     FitViewport viewport;
 
     Sprite bucketSprite;
+
+    Vector2 touchPos;
+
+    Array<Sprite> dropSprites;
+
+    flaot dropTimer;
 
     @Override
     public void create() {
@@ -41,6 +50,9 @@ public class Main implements ApplicationListener {
         bucketSprite.setSize(64, 64);
         bucketSprite.setPosition(viewport.getWorldWidth() / 2 - 32, 20);
         
+        touchPos = new Vector2(); //Using Vector2 prevents the triggering of garbage collector that prevents lag spikes
+
+        dropSprites = new Array<>();
     }
 
     @Override
@@ -56,15 +68,38 @@ public class Main implements ApplicationListener {
     }
 
     private void input(){
-        float speed = .25f;
+        float speed = 250f;
+        float delta = Gdx.graphics.getDeltaTime();
 
         if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
-            bucketSprite.translateX(speed);
+            bucketSprite.translateX(speed * delta);
+        } else if (Gdx.input.isKeyPressed(Keys.LEFT)) {
+            bucketSprite.translateX(-speed * delta);
+        }
+
+        if (Gdx.input.isTouched()) {
+            touchPos.set(Gdx.input.getX(), Gdx.input.getY());
+            viewport.unproject(touchPos);
+            bucketSprite.setCenterX(touchPos.x);
         }
     }
 
     private void logic(){
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
 
+        float bucketWidth = bucketSprite.getWidth();
+        float bucketHeight = bucketSprite.getHeight();
+
+        bucketSprite.setX(MathUtils.clamp(bucketSprite.getX(), 0, worldWidth - bucketWidth));
+
+        float delta = Gdx.graphics.getDeltaTime();
+
+        for (Sprite dropSprite : dropSprites) {
+            dropSprite.translateY(-180f * delta);
+        }
+
+        createDroplet();
     }
 
     private void draw(){
@@ -79,7 +114,24 @@ public class Main implements ApplicationListener {
         spriteBatch.draw(backgroundTexture, 0, 0, worldWidth, worldHeight);
         bucketSprite.draw(spriteBatch);  // Draw bucket in middle bottom
 
+        for (Sprite dropSprite: dropSprites){
+            dropSprite.draw(spriteBatch);
+        }
+
         spriteBatch.end();
+    }
+
+    private void createDroplet(){
+        float dropWidth = 1;
+        float dropHeight = 1;
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+
+        Sprite dropSprite = new Sprite(dropTexture);
+        dropSprite.setSize(64, 64);
+        dropSprite.setX(MathUtils.random(0f, worldWidth - dropWidth));
+        dropSprite.setY(worldHeight);
+        dropSprites.add(dropSprite);
     }
     @Override
     public void pause() {
