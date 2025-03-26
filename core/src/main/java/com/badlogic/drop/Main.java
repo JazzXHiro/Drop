5855b363e11bd5d54941ad43c27ee6b05e774e8d
JazzXHiro <73639405+JazzXHiro@users.noreply.github.com>
2025-03-26 16:44:21 +0530

    first commit

---
+++ core/src/main/java/com/badlogic/drop/Main.java
@@ -0,0 +1,103 @@
+package com.badlogic.drop;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main implements ApplicationListener {
+    Texture backgroundTexture;
+    Texture bucketTexture;
+    Texture dropTexture;
+    Sound dropSound;
+    Music music;
+
+    SpriteBatch spriteBatch;
+    FitViewport viewport;
+
+    Sprite bucketSprite;
+
+    @Override
+    public void create() {
+        backgroundTexture = new Texture("background.png");
+        bucketTexture = new Texture("bucket.png");
+        dropTexture = new Texture("drop.png");
+        dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.mp3"));
+        music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+
+        spriteBatch = new SpriteBatch();
+        viewport = new FitViewport(800, 480);  // More standard dimensions
+        viewport.getCamera().position.set(viewport.getWorldWidth() / 2, viewport.getWorldHeight() / 2, 0);
+
+        bucketSprite = new Sprite(bucketTexture);
+        bucketSprite.setSize(64, 64);
+        bucketSprite.setPosition(viewport.getWorldWidth() / 2 - 32, 20);
+        
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height, true);
+    }
+
+    @Override
+    public void render() {
+        input();
+        logic();
+        draw();
+    }
+
+    private void input(){
+        float speed = .25f;
+
+        if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
+            bucketSprite.translateX(speed);
+        }
+    }
+
+    private void logic(){
+
+    }
+
+    private void draw(){
+        ScreenUtils.clear(Color.BLACK);
+        viewport.apply();
+        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
+        spriteBatch.begin();
+
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+
+        spriteBatch.draw(backgroundTexture, 0, 0, worldWidth, worldHeight);
+        bucketSprite.draw(spriteBatch);  // Draw bucket in middle bottom
+
+        spriteBatch.end();
+    }
+    @Override
+    public void pause() {
+        // Invoked when your application is paused.
+    }
+
+    @Override
+    public void resume() {
+        // Invoked when your application is resumed after pause.
+    }
+
+    @Override
+    public void dispose() {
+        backgroundTexture.dispose();
+        bucketTexture.dispose();
+        dropTexture.dispose();
+        dropSound.dispose();
+        music.dispose();
+        spriteBatch.dispose();
+    }
+}
